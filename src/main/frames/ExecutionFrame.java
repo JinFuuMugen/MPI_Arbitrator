8bd86f471fa7e1a6458cdd108e245c0f77629220
Kalinin Alexey <leshakalinin93@gmail.com>
2023-05-12 17:00:12 +0300

    reorganized structure and made some minor changes

--- src/main/frames/ExecutionFrame.java
+++ src/main/frames/ExecutionFrame.java
@@ -1,3 +1,7 @@
+package src.main.frames;
+
+import src.main.cmd.Arbitrator;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -5,35 +9,27 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
 
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
-public class ParamFrame extends JFrame {
-
-    public static final JFrame Frame = GetParamFrame();
-
-    static JFrame GetParamFrame() {
+public class ExecutionFrame extends JFrame {
+    public static JFrame GetExecutionFrame() {
         JFrame frame = new JFrame("MPI Parameters");
         frame.setSize(1000, 500);
         String[] paramTableColumns = { "Parameter name", "Description", "Value" };
         Object[][] paramData = {
-                { "File", "Name of C/CPP MPI file", "File is not choosen!" },
-                { "Path", "Directory of C/CPP MPI file", "File is not choosen!" },
+                { "File", "Name of C/CPP MPI file", Arbitrator.getSelectedFileName() },
+                { "Path", "Directory of C/CPP MPI file", Arbitrator.getSelectedFilePath() },
                 { "-localonly", "Specifies that MPI processes may only be launched on the local machine", "" },
                 { "-envall", "Propagates all environment variables from the launching machine to the MPI processes",
                         "" },
@@ -80,7 +76,7 @@ public class ParamFrame extends JFrame {
         TableCellRenderer renderer = new DefaultTableCellRenderer() {
             @Override
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                    boolean hasFocus, int row, int column) {
+                                                           boolean hasFocus, int row, int column) {
                 Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
                 String val = (String) value;
                 if (val.equals("File is not choosen!")) {
@@ -103,64 +99,45 @@ public class ParamFrame extends JFrame {
         JScrollPane scrollPane = new JScrollPane(table);
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        // create file chooser button
-        JButton chooseFileButton = new JButton("Choose file");
-        chooseFileButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("C/C++ Files", "c", "cpp");
-                fileChooser.setFileFilter(filter);
-                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-                int result = fileChooser.showOpenDialog(frame);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = fileChooser.getSelectedFile();
-                    String fileName = selectedFile.getName();
-                    String filePath = selectedFile.getParent();
-                    model.setValueAt(fileName, 0, 2); // update filename row
-                    model.setValueAt(filePath, 1, 2); // update path row
-                }
-            }
-        });
-        JPanel topPanel = new JPanel();
-        topPanel.add(chooseFileButton);
-        frame.add(topPanel, BorderLayout.NORTH);
 
-        // create run button
         JButton runButton = new JButton("Run");
-        runButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String fileName = (String) model.getValueAt(0, 2);
-                String filePath = (String) model.getValueAt(1, 2);
-                String stdMpi = " -l msmpi -l stdc++ -L \"C:\\Program Files (x86)\\Microsoft SDKs\\MPI\\Lib\\x64\" -I \"C:\\Program Files (x86)\\Microsoft SDKs\\MPI\\Include\"";
-                // String command = "g++ -o " + fileName + " " + filePath + stdMpi;
-                String command = "cmd.exe /c cd /d " + filePath + " && " + "g++ -o javaTest.exe " + fileName + stdMpi;
-                // g++ -o hello.exe hello.cpp -l msmpi -L "C:\Program Files (x86)\Microsoft
-                // SDKs\MPI\Lib\x64" -I "C:\Program Files (x86)\Microsoft SDKs\MPI\Include"
-                try {
-                    // Run command and capture output
-                    Process process = Runtime.getRuntime().exec(command);
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                    StringBuilder builder = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        builder.append(line);
-                        builder.append(System.getProperty("line.separator"));
-                    }
-                    String output = builder.toString();
-
-                    // Create new window and display output
-                    JFrame frame = new JFrame("Command Result");
-                    JTextArea textArea = new JTextArea(output);
-                    JScrollPane scrollPane = new JScrollPane(textArea);
-                    frame.add(scrollPane);
-                    frame.setSize(400, 300);
-                    frame.setVisible(true);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
+        runButton.addActionListener(e -> {
+            String stdMpi = " -l msmpi -l stdc++ -L \"C:\\Program Files (x86)\\Microsoft SDKs\\MPI\\Lib\\x64\" -I \"C:\\Program Files (x86)\\Microsoft SDKs\\MPI\\Include\"";
+            // String command = "g++ -o " + fileName + " " +
+            // ArbitratorApplication.selectedFilePath + stdMpi;
+            String command = "cmd /c cd /d " + Arbitrator.getSelectedFilePath()
+                    + " && echo Working directory changed to: " + Arbitrator.getSelectedFilePath()
+                    + " && " + "g++ " + Arbitrator.getSelectedFileName() + stdMpi
+                    + " && echo Compiled!  && echo Program output:"
+                    + " && mpiexec -host 192.168.1.101 -np 3 a.exe";
+            // g++ -o hello.exe hello.cpp -l msmpi -L "C:\Program Files (x86)\Microsoft
+            // SDKs\MPI\Lib\x64" -I "C:\Program Files (x86)\Microsoft SDKs\MPI\Include"
+            try {
+                Process process = Runtime.getRuntime().exec(command);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                    builder.append(System.getProperty("line.separator"));
                 }
+                String output = builder.toString();
+
+                JFrame frame1 = new JFrame("Command Result");
+                JTextArea textArea = new JTextArea(output);
+                textArea.setFont(new Font("Arial", Font.PLAIN, 15));
+                textArea.setEditable(false);
+                JScrollPane scrollPane1 = new JScrollPane(textArea);
+                frame1.add(scrollPane1);
+                frame1.setSize(400, 300);
+                frame1.setVisible(true);
+                frame1.setAlwaysOnTop(true);
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
+
+            frame.setSize(500, 500);
+            frame.setVisible(true);
         });
 
         JPanel buttonPanel = new JPanel();
---
+++ src/main/visuals/ClearSelectionsButton.java
@@ -0,0 +1,17 @@
+package src.main.visuals;
+
+import src.main.frames.AddressesFrame;
+
+import javax.swing.*;
+import java.util.List;
+
+public class ClearSelectionsButton extends JButton {
+    public ClearSelectionsButton(String name, List<String[]> data) {
+        super(name);
+        addActionListener(e -> {
+            for (int i = 0; i < data.size(); i++) {
+                AddressesFrame.table.getModel().setValueAt(false, i, 2);
+            }
+        });
+    }
+}
