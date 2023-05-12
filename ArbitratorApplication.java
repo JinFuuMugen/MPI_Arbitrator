2919f2b87154c03c57679ccea92a89c63c942c19
Kalinin Alexey <leshakalinin93@gmail.com>
2023-05-12 15:07:32 +0300

    made file opening first

--- ArbitratorApplication.java
+++ ArbitratorApplication.java
@@ -1,8 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
